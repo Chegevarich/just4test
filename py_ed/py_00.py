11ebae6ae6a9e4d06caf9869481738389177fd79
Chegevarich <chegevarich@gmail.com>
2015-10-10 18:20:55 +0300

    first one

---
+++ py_ed/py_00.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import os
+
+list_dir = os.listdir(".")
+
+f2 = open("ffw", 'w')
+f2.write('')
+f2.close()
+
+for name in list_dir:
+	f = open(name)
+	pys = f.read().count('python') 
+
+	if pys > 0:
+		print('got ' + str(pys) + ' "python\'s" in file ' + name)
+		f2 = open("ffw", 'a')
+		f2.write(str(name) + str(' --> ') + str(pys) + '\r\n')
+		f2.close()
